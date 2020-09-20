0eab08998d856c01e118c9f9ac3fcdcb63526c8c
alpha74 <amanalphakumar@gmail.com>
2020-09-20 23:55:05 +0530

    Add url routes.

--- ratings/urls.py
+++ ratings/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path( '', views.home, name="blog-home" ),
-
+    path( '', views.home, name="ratings-home" ),
+	path( 'rate/driver', views.rate_driver, name ="rate_driver" ),
+	path( 'rate/passenger', views.rate_passenger, name ="rate_passenger" ),
+	path( 'view/driver', views.rating_driver, name ="view_rating_driver"),
+	path( 'view/passenger', views.rating_passenger, name ="view_rating_passenger"),
 ]
