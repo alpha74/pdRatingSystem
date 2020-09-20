949582f7e1661ef23f19eb8d97338d16fa7d8e44
alpha74 <amanalphakumar@gmail.com>
2020-09-20 14:09:26 +0530

    Create models Passenger, Driver.

---
+++ ratings/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+"""
+	Passenger object
+		- name : text
+		- rating : decimal number
+"""
+class Passenger( models.Model ):
+	name = models.Charfield( max_length=30)
+	rating = models.DecimalField( max_digits=2, decimal_places=1,default = 3.5) 
+
+	def __str__( self ):
+		return self.name
+
+
+"""
+	Driver object
+		- name : text
+		- rating : decimal number
+		- trips : int : number of trips done by driver
+"""
+class Driver( models.Model ):
+	name = models.Charfield( max_length= 30 )
+	rating = models.DecimalField( max_digits = 2, decimal_places = 1, default = 3.5 )
+	trips = models.DecimalField( max_digits = 10, decimal_places = 0, default = 0 )
+	
+	def __str__( self ):
+		return self.name 
